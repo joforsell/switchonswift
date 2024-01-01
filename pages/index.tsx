5a6cc147c524a9074e7082cd02613dfa61bdf95e
Johan Forsell <forsell.johan@hotmail.com>
2024-01-01 22:50:58 +0100

    Add a year in blog post

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Layout, Tab } from '../components/Layout'
 function Home() {
   return (
     <Layout activeTab={Tab.Home}>
-      <h1 className='text-center pt-10'>One day I will be a beautiful, fleshed out home page. Not yet though.</h1>
+      <h1 className='text-center pt-10'>One day this will be a beautiful, fleshed out home page. Not yet though.</h1>
     </Layout>
   )
 }
